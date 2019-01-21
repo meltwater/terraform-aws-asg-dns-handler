0f88236cd32ec992d31c148fcffb7bdd88e28118
apoorva-marisomaradhya <45768960+apoorva-marisomaradhya@users.noreply.github.com>
2019-01-21 13:16:04 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "autoscale_handler_unique_identifier" {
   description = "asg_dns_handler"
 }
 
-variable "vpc_name" {}
+variable "vpc_name" {
+  description = "The name of the VPC"
+}
 
-variable "autoscale_route53zone_arn" {}
+variable "autoscale_route53zone_arn" {
+  description = "The ARN of route53 zone associated with autoscaling group"
+}
